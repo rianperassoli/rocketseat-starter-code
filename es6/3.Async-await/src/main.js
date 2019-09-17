5d3ba457b7acb630eac1a94e37e88fbe2196238b
Rian Perassoli <rian@torfresma.com.br>
2019-09-17 14:02:56 -0300

    desafios async await

--- es6/3.Async-await/src/main.js
+++ es6/3.Async-await/src/main.js
@@ -11,17 +11,55 @@
 
 // execute();
 
-import axios from 'axios'
+// import axios from 'axios'
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`)
-            console.log(response)
-        } catch (err) {
-            console.warn(`Erro na API: ${err}`)
-        }
-    }
-}
+// class Api {
+//     static async getUserInfo(username) {
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`)
+//             console.log(response)
+//         } catch (err) {
+//             console.warn(`Erro na API: ${err}`)
+//         }
+//     }
+// }
 
-Api.getUserInfo('rianperassoli')
+// Api.getUserInfo('rianperassoli')
+
+
+//EXERCICIO
+// const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+// async function umPorSegundo() {
+//     try {
+//         await delay() 
+//         console.log('1s')
+
+//         await delay() 
+//         console.log('2s')
+
+//         await delay() 
+//         console.log('3s')
+//     } catch (error) {
+//         console.warn(`Erro: ${error}`)
+//     }
+// }
+
+// umPorSegundo();
+
+//EXERCICIO 2
+// import axios from 'axios';
+
+// async function getUserFromGithub(user) {
+
+//     try {
+//         const response = await axios.get(`https://api.github.com/users/${user}`)
+
+//         console.log(response);
+//     } catch (err) {
+//         console.log('Usuário não existe');
+//     }
+// }
+
+// getUserFromGithub('diego3g');
+// getUserFromGithub('diego3g124123');
